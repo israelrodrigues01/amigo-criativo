6d874a26dfd9e555680969d70b9e2377a3b884d5
israelrodrigues01 <ciceroisral428@gmail.com>
2023-12-08 15:02:39 -0300

    first commit

---
+++ assets/js/images.js
@@ -0,0 +1,54 @@
+var arrayImages = [
+    {
+        "image": "assets/images/A gente precisa de poesia dentro da gente. De alma perfumada e riso de criança.jpg",
+        "description": "A gente precisa de poesia dentro da gente. De alma perfumada e riso de criança",
+    },
+    {
+        "image": "assets/images/Eu vim do céu por causa do amor !.jpg",
+        "description": "Eu vim do céu por causa do amor !",
+    },
+    {
+        "image": "assets/images/família.jpg",
+        "description": "Família",
+    },
+    {
+        "image": "assets/images/filho.jpg",
+        "description": "Filho",
+    },
+    {
+        "image": "assets/images/filhos.jpg",
+        "description": "Filhos",
+    },
+    {
+        "image": "assets/images/Gerando mais um amor!.jpg",
+        "description": "Gerando mais um amor!",
+    },
+    {
+        "image": "assets/images/Meu lampiãozinho.jpg",
+        "description": "Meu lampiãozinho",
+    },
+    {
+        "image": "assets/images/Os dias de lutas foram intensos, mas a vitória só foi possível porque Deus estava ao nosso lado. Só gratidão.jpg",
+        "description": "Os dias de lutas foram intensos, mas a vitória só foi possível porque Deus estava ao nosso lado. Só gratidão",
+    },
+    {
+        "image": "assets/images/Pai ♥ Mãe.jpg",
+        "description": "Pai ♥ Mãe",
+    },
+    {
+        "image": "assets/images/Por isso eu prefiro sorrisos, e os presentes que a vida trouxe pra perto de mim..jpg",
+        "description": "Por isso eu prefiro sorrisos, e os presentes que a vida trouxe pra perto de mim.",
+    },
+    {
+        "image": "assets/images/Quando há amor no seio de um lar, nada rompe os laços entre pai, mãe e filhos!.jpg",
+        "description": "Quando há amor no seio de um lar, nada rompe os laços entre pai, mãe e filhos!",
+    },
+    {
+        "image": "assets/images/Servir a Deus é um grande privilégio..jpg",
+        "description": "Servir a Deus é um grande privilégio.",
+    },
+    {
+        "image": "assets/images/Ter um lugar pra ir é lar. Ter alguém para amar é família. Ter os dois é benção..jpg",
+        "description": "Ter um lugar pra ir é lar. Ter alguém para amar é família. Ter os dois é benção.",
+    },
+];
---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    let html = '';
+    arrayImages.forEach(image => {
+        html += `
+            <li class="item">
+                <div>
+                    <div class="polaroid"><img src="${image.image}">
+                        <div class="caption">${image.description}</div>
+                    </div>
+                </div>
+            </li>
+        `;
+    })
+    $('#photos').append(html);
+});
