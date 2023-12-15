f0ac5ec0f74cde499487b0f42327c20fb05f732e
israelrodrigues01 <ciceroisral428@gmail.com>
2023-12-15 12:48:19 -0300

    adiciono vídeo apresentativo e link para o polaroid

--- assets/js/envelope.js
+++ assets/js/envelope.js
@@ -1,7 +1,13 @@
 $('#card').on('click', function () {
     $('#envelope').addClass('active');
+
+    setTimeout(()=>{
+        $('.presente-extra').removeClass('d-none');
+        $('.rubber').text('Para Raquel')
+    }, 1)
 });
 
 $('.closed-envelope').on('click', function () {
     $('#envelope').removeClass('active');
+    $('.button-extra').addClass('d-none');
 });
